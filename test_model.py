7013d3cf0f50af229ee74256ca4171a3afb88241
Tom Dawson <tomdawson720@gmail.com>
2022-05-22 17:11:15 +0100

    Adding can_allocate tests

--- test_model.py
+++ test_model.py
@@ -8,27 +8,49 @@ tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
 
-def test_allocating_to_a_batch_reduces_the_available_quantity():
-    test_batch = Batch(
-        reference="Batch-0001", sku="small_chair", quantity=30, eta=date.today()
+def make_test_batch_and_line(sku: str, quantity_batch: int, quantity_line: int):
+    return (
+        Batch(
+            reference="Batch-0001", sku=sku, quantity=quantity_batch, eta=date.today()
+        ),
+        OrderLine(order_id="my_reference", sku=sku, quantity=quantity_line),
     )
-    line = OrderLine(order_id="my_reference", sku="small_chair", quantity=7)
 
-    test_batch.allocate(line)
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    test_batch, test_line = make_test_batch_and_line("small_chair", 30, 7)
+    test_batch.allocate(test_line)
 
     assert test_batch.available_units == 23
 
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
+    test_batch, small_line = make_test_batch_and_line("small_chair", 30, 7)
+
+    assert test_batch.can_allocate(small_line) is True
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+    test_batch, large_line = make_test_batch_and_line("small_chair", 30, 35)
+
+    assert test_batch.can_allocate(large_line) is False
 
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail("todo")
+    test_batch, equal_line = make_test_batch_and_line("small_chair", 30, 30)
+
+    assert test_batch.can_allocate(equal_line) is True
+
+
+def test_cannot_allocate_if_skus_do_not_match():
+    test_batch = Batch(
+        reference="Batch-0001", sku="small_chair", quantity=30, eta=date.today()
+    )
+    different_sku_line = OrderLine(
+        order_id="my_reference", sku="small_table", quantity=7
+    )
+
+    assert test_batch.can_allocate(different_sku_line) is False
 
 
 def test_prefers_warehouse_batches_to_shipments():
